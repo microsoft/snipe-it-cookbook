ae039eb1e45bfa9b43dfd9044ccac9d7c8d6b4fa
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-08-14 16:47:48 -0700

    Add tests for the snipeit nginx config

--- test/integration/default/nginx_test.rb
+++ test/integration/default/nginx_test.rb
@@ -1,5 +1,11 @@
 # # encoding: utf-8
 
+php_socket_path = if os.family == 'debian'
+                    '/run/php/php7.0-fpm'
+                  else
+                    '/var/run/php-fpm/php-fpm'
+                  end
+
 describe service('nginx') do
   it { should be_running }
   it { should be_enabled }
@@ -7,4 +13,9 @@ end
 
 describe port('80') do
   it { should be_listening }
+end
+
+describe file('/etc/nginx/sites-available/snipeit') do
+  its('content') { should include('root /var/www/snipeit/public') }
+  its('content') { should include('unix:' + php_socket_path + '.sock') }
 end
