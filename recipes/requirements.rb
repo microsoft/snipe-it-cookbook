8d33e73de34a60b44ac1fc4149fc37e3651cfde7
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-08-14 15:44:09 -0700

    Add requirements recipe for installing the repo

---
+++ recipes/requirements.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook:: snipe-it-cookbook
+# Recipe:: requirements
+#
+# The MIT License (MIT)
+#
+# Copyright:: 2018, Microsoft
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+package 'git' do
+  only_if { platform_family?('rhel') }
+end
+
+directory '/var/www' do
+  user node['nginx']['user']
+  group node['nginx']['group']
+end
+
+git node['snipeit']['path'] do
+  repository 'git://github.com/snipe/snipe-it'
+  revision 'master'
+  action :sync
+  user node['nginx']['user']
+  group node['nginx']['group']
+end
