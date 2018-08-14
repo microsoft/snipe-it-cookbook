c8782fdc05c29a555932d7ca549d3a4dddf01c99
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-08-14 15:43:39 -0700

    Add recipe to install required packages and dependencies for Laravel

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,14 @@
 default['snipeit']['path'] = '/var/www/snipeit'
 default['snipeit']['doc_root'] = node['snipeit']['path'] + '/public'
+
+default['snipeit']['php']['packages'] = [
+  'php-bcmath',
+  'php-curl',
+  'php-fpm',
+  'php-gd',
+  'php-ldap',
+  'php-mbstring',
+  'php-mysql',
+  'php-pdo',
+  'php-zip',
+]
