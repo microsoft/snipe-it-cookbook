5d4603a74758209a14688bb7e5fe2c80e4135870
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-08-15 09:29:49 -0700

    Add supports Ubuntu and CentOS in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs and configures Snipe-IT asset/license management system.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
+supports 'ubuntu'
+supports 'centos'
 
 issues_url 'https://github.com/Microsoft/snipe-it-cookbook/issues'
 source_url 'https://github.com/Microsoft/snipe-it-cookbook'
