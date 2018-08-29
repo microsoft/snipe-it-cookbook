b545d6b72b11590c8f1aa105cd7ad1a8c77f406f
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-08-29 10:39:31 -0700

    Add default locale and time zone attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
 default['snipeit']['path'] = '/var/www/snipeit'
 default['snipeit']['debug'] = false
 default['snipeit']['doc_root'] = node['snipeit']['path'] + '/public'
+default['snipeit']['locale'] = 'en'
+default['snipeit']['timezone'] = 'US/Pacific'
 
 default['snipeit']['php']['packages'] = [
   'php-bcmath',
