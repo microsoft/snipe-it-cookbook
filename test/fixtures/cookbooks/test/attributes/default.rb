968a0c7bc4b544e381b404e5ad6fa063cca5eae6
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-09-07 16:05:57 -0700

    Use VSTS ENV variables formatting

--- test/fixtures/cookbooks/test/attributes/default.rb
+++ test/fixtures/cookbooks/test/attributes/default.rb
@@ -1,9 +1,9 @@
-default['snipeit']['php']['app_key'] = ENV['APP_KEY']
+default['snipeit']['php']['app_key'] = "$(APP_KEY)"
 
-default['snipeit']['database']['domain'] = ENV['MYSQL_SERVER_DOMAIN']
-default['snipeit']['database']['hostname'] = ENV['MYSQL_SERVER_HOSTNAME']
-default['snipeit']['database']['username'] = ENV['MYSQL_SERVER_USERNAME']
-default['snipeit']['database']['password'] = ENV['MYSQL_SERVER_PASSWORD']
+default['snipeit']['database']['domain'] = "$(MYSQL_SERVER_DOMAIN)"
+default['snipeit']['database']['hostname'] = "$(MYSQL_SERVER_HOSTNAME)"
+default['snipeit']['database']['username'] = "$(MYSQL_SERVER_USERNAME)"
+default['snipeit']['database']['password'] = "$(MYSQL_SERVER_PASSWORD)"
 
-default['snipeit']['smtp']['username'] = ENV['SMTP_USERNAME']
-default['snipeit']['smtp']['password'] = ENV['SMTP_PASSWORD']
+default['snipeit']['smtp']['username'] = "$(SMTP_USERNAME)"
+default['snipeit']['smtp']['password'] = "$(SMTP_PASSWORD)"
