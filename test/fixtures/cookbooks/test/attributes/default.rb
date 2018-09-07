d07019fdecf6d1090bb352d232194bdca07d5825
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-09-07 15:43:46 -0700

    Set default attributes for SMTP in the test cookbook to default attributes

--- test/fixtures/cookbooks/test/attributes/default.rb
+++ test/fixtures/cookbooks/test/attributes/default.rb
@@ -4,3 +4,6 @@ default['snipeit']['database']['domain'] = ENV['MYSQL_SERVER_DOMAIN']
 default['snipeit']['database']['hostname'] = ENV['MYSQL_SERVER_HOSTNAME']
 default['snipeit']['database']['username'] = ENV['MYSQL_SERVER_USERNAME']
 default['snipeit']['database']['password'] = ENV['MYSQL_SERVER_PASSWORD']
+
+default['snipeit']['smtp']['username'] = ENV['SMTP_USERNAME']
+default['snipeit']['smtp']['password'] = ENV['SMTP_PASSWORD']
