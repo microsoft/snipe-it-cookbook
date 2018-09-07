ac95b50a4c3d3c5b1d31ee1be16ddf9e9e1e46ce
Eammon Hanlon <eammon.hanlon@microsoft.com>
2018-09-07 15:27:00 -0700

    Add test/fixtures/cookbooks/test to wrap snipe-it for testing.

---
+++ test/fixtures/cookbooks/test/attributes/default.rb
@@ -0,0 +1,6 @@
+default['snipeit']['php']['app_key'] = ENV['APP_KEY']
+
+default['snipeit']['database']['domain'] = ENV['MYSQL_SERVER_DOMAIN']
+default['snipeit']['database']['hostname'] = ENV['MYSQL_SERVER_HOSTNAME']
+default['snipeit']['database']['username'] = ENV['MYSQL_SERVER_USERNAME']
+default['snipeit']['database']['password'] = ENV['MYSQL_SERVER_PASSWORD']
---
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -0,0 +1,6 @@
+name 'test'
+maintainer 'Microsoft'
+maintainer_email 'chef@microsoft.com'
+license 'MIT'
+version '0.1.0'
+depends 'snipe-it'
